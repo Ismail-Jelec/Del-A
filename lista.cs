01d6b008fb209e4f0105351d3cf55f8da5dfb6ba
Ismail-Jelec <ismail.jelec@gmail.com>
2026-09-10 10:52:03 +0200

    first commit

---
+++ lista.cs
@@ -0,0 +1,23 @@
+Console.WriteLine("");
+Console.WriteLine("Mjölk - 15kr");
+Console.WriteLine("Bröd - 32kr");
+Console.WriteLine("Ost - 89kr");
+
+static void Buy()
+{
+    Console.ReadLine();
+
+}
+
+
+
+List <Store> items = [];
+
+
+string[] menu = [
+    "Vad vill du köpa?",
+    "Mjölk - 15kr",
+    "Bröd - 32kr", 
+    "Ost - 89kr",
+    "Ajvar - 30kr",
+];
